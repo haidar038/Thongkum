0ffacbaa0378ab3da053fa61b261c615bb4bc206
unknown <haidar038@gmail.com>
2023-02-07 14:30:44 +0700

    Initial Commit

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
+import os, sys
+
+sys.path.append(os.path/abs(os.path.join(os.path.dirname(__file__), "..")))
+
 from web import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    app.run()
